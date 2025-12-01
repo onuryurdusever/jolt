85e8b927870405fc9d539bc9b49374e261dd9f3e
onuryurdusever <onuryurdusever@gmail.com>
2025-12-01 14:10:02 +0300

    Initial Commit

---
+++ joltTests/joltTests.swift
@@ -0,0 +1,17 @@
+//
+//  joltTests.swift
+//  joltTests
+//
+//  Created by Onur Yurdusever on 1.12.2025.
+//
+
+import Testing
+@testable import jolt
+
+struct joltTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
